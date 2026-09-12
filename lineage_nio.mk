cc1f3c287b00fe86d5c013f2427899494019cffc
Deivid21 <david.parra.ignacio@gmail.com>
2026-09-11 23:05:51 -0300

    Revert "nio: move product inheritance to common"
    This reverts commit 99e1921efec8606dcd2a886c1928223ff97a2f3a.
    
    Change-Id: I7209733f81c9fdf10f185ec6d03e009108f8e9ea

--- lineage_nio.mk
+++ lineage_nio.mk
@@ -4,13 +4,22 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit from nio device
 $(call inherit-product, device/motorola/nio/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_nio
 PRODUCT_DEVICE := nio
+PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g(100)
+PRODUCT_MANUFACTURER := motorola
 
 # Build info
 PRODUCT_BUILD_PROP_OVERRIDES += \
