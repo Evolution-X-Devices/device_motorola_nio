6ca351a20a9e300b4ac2a2a2b181a6488a9f581c
Deivid21 <david.parra.ignacio@gmail.com>
2026-09-11 22:56:22 -0300

    nio: Rename to moto g(100)
    * Match with Global name
    
    Change-Id: I3899f210b4c024486ae3164b1d8aaaccf235a228

--- lineage_nio.mk
+++ lineage_nio.mk
@@ -10,7 +10,7 @@ $(call inherit-product, device/motorola/nio/device.mk)
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_nio
 PRODUCT_DEVICE := nio
-PRODUCT_MODEL := XT2125-4
+PRODUCT_MODEL := moto g(100)
 
 # Build info
 PRODUCT_BUILD_PROP_OVERRIDES += \
