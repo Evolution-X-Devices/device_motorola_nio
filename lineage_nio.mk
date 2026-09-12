3f1cdea5c58beb3d44d0f755f0db029e0b9f8ef6
Deivid21 <david.parra.ignacio@gmail.com>
2026-09-11 23:36:41 -0300

    nio: Update product build to S1RTS32.41-20-16-1-11
    Change-Id: I42e68b8664979c2d17ee2ea12c46049df871344a

--- lineage_nio.mk
+++ lineage_nio.mk
@@ -25,6 +25,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 # Build info
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="nio_retcn-user 12 S1RN32.55-16-13 fce21a-b9f4e9 release-keys" \
-    BuildFingerprint=motorola/nio_retcn/nio:12/S1RN32.55-16-13/fce21a-b9f4e9:user/release-keys \
-    DeviceProduct=nio_retcn
+    BuildDesc="nio_retail-user 12 S1RTS32.41-20-16-1-11 3797e3-c267bf release-keys" \
+    BuildFingerprint=motorola/nio_retail/nio:12/S1RTS32.41-20-16-1-11/3797e3-c267bf:user/release-keys \
+    DeviceProduct=nio_retail
